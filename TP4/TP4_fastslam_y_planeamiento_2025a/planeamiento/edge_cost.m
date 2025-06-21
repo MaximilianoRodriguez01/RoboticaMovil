4ee20cbfb76ac11220669ba426c4c84bdffe263f
MaximilianoRodriguez01 <masrodriguez@fi.uba.ar>
2025-06-21 16:28:22 -0300

    correcion

--- TP4/TP4_fastslam_y_planeamiento_2025a/planeamiento/edge_cost.m
+++ TP4/TP4_fastslam_y_planeamiento_2025a/planeamiento/edge_cost.m
@@ -1,13 +1,13 @@
 function cost = edge_cost(parent, child, map)
 
-  threshold = 0.9;
+  threshold = 0.8;
 
   % Chequear si la celda hija está ocupada
   if map(child(1), child(2)) < threshold
       % Calcular distancia euclidiana
-  cost = sqrt((child(1) - parent(1))^2 + (child(2) - parent(2))^2) + 10 * map(child(1), child(2));
+  cost = sqrt((child(1) - parent(1))^2 + (child(2) - parent(2))^2) + 100 * map(child(1), child(2));
   else
       % Penalizar celdas ocupadas
-      cost = 1e3;
+      cost = 1e6;
   end
 end
